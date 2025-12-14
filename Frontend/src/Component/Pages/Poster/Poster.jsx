800e6c344b5e284a3911d24d4323e6b9d8866c0c
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2025-12-14 21:59:37 +0545

    poster edit

--- Frontend/src/Component/Pages/Poster/Poster.jsx
+++ Frontend/src/Component/Pages/Poster/Poster.jsx
@@ -3,9 +3,19 @@ import React from 'react'
 const Poster = () => {
   return (
     <div> 
-       <div>
-        <img className='w-full' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY4BIvsbNpokbdGOsg9HXq9mooSi29USFOkw&s'></img>
+       <div className=' flex flex-col '>
+             <div>
+                  <img className=' h-150 w-full rounded-2xl' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY4BIvsbNpokbdGOsg9HXq9mooSi29USFOkw&s'></img>
+
+             </div>
+
+        <div className=''>
+
+        </div>
         
+           <div className='h-70 w-full  rounded-xl   top-10/11  bg-blue-300'>
+               <p>This is the title </p>
+            </div>
        </div>
     </div>
   )
