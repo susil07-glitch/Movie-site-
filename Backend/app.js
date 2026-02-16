26610c77fc405d50d6776074ba5b38fb07377a43
Sushil Sharma <sushilsharma@Sushils-MacBook-Air.local>
2026-02-17 01:24:56 +0545

    updating cors

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,14 @@ const express =require('express')
 
 
 const app=express();
+/*---------Cors------------------*/
+const cors=require('cors')
+
+app.use(cors({
+     origin: "http://localhost:5173",
+    credentials: true
+}))
+
 
 /*---------MiddleWare-------------*/
 
